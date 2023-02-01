6dad800499c0256f3a0a4d5b01003a02ccfca785
Lane Schwartz <dowobeha@gmail.com>
2023-02-01 11:21:48 -0900

    Added simplest possible C++ program

---
+++ week2/simple.cpp
@@ -0,0 +1,28 @@
+int main() {
+
+  return 0;
+}
+
+
+/*
+  This code represents the simplest legal C++ program.
+
+  When you compile a C++ program, 
+    the compiler will look for a function called main with return type int.
+
+  When you run the program, this main function is where the code starts running.
+
+  Remember that every function contains four parts:
+
+  * a return type 
+  * a name
+  * a parameter list
+  * a body
+
+  In the main function above:
+  * the return type is int
+  * the name is main
+  * the parameter list is empty (represented by the empty parentheses)
+  * the body is everything between the curly braces
+
+ */
