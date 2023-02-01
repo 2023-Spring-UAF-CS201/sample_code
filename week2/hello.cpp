b0b632ace32ddf5d36e9ebfa3bd6640ebe20e6fd
Lane Schwartz <dowobeha@gmail.com>
2023-02-01 11:23:15 -0900

    Hello, world

---
+++ week2/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>    // Provides access to std::cout
+
+using std::cout;       // Allows us to write cout rather than std::cout
+
+
+int main() {
+
+  cout << "Hello, world\n";
+
+  return 0;
+}
