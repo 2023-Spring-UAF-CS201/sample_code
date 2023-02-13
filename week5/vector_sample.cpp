c384a072f356aa81161433635db335f72155cf59
Lane Schwartz <dowobeha@gmail.com>
2023-02-13 09:59:51 -0900

    Sample code using a vector and a ranged for loop

--- week5/vector_sample.cpp
+++ week5/vector_sample.cpp
@@ -1,8 +1,21 @@
 #include <vector>
 #include <iostream>
 
+using std::cout;
+using std::endl;
+using std::vector;
 
 int main() {
 
+  vector<int> numbers;
+
+  numbers.push_back(17);
+  numbers.push_back(-34);
+  numbers.push_back(0);
+
+  for (int n : numbers) {
+    cout << n << endl;
+  }
+  
   return 0;
 }
