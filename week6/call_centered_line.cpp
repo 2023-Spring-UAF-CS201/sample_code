7193aadfdeaaabda1b9fe6f8e22792181511c632
Lane Schwartz <dowobeha@gmail.com>
2023-02-20 09:51:02 -0900

    Example of calling a function

---
+++ week6/call_centered_line.cpp
@@ -0,0 +1,24 @@
+
+string centered_line(char c, int width, char p, int padding) {
+
+  string result;
+
+  // Does something
+  result = "My cool correct amazing result";
+
+  return result;
+}
+
+
+int main() {
+
+  cout << centered_line('*', 7, '_', 3) << endl;
+
+  char middle_char;
+  cin >> middle_char;
+
+  string this_result{ centered_line(middle_char, 7, '_', 3) };
+
+  cout << this_result << endl;
+  
+}
