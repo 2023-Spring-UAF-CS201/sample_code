90f57692652ec1de65c456d26974f86cfa66784a
Lane Schwartz <dowobeha@gmail.com>
2023-04-19 10:16:19 -0800

    References

---
+++ week14/references.cpp
@@ -0,0 +1,22 @@
+
+class Library {
+  ///...
+};
+
+
+int booksInLibrary(Library& library) {
+
+
+  return 27050;
+}
+
+int main() {
+
+  Library l;
+  // add a bunch of books
+
+  int x = booksInLibrary(library);
+  
+  
+  return 0;
+}
