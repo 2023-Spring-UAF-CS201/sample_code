3168635275dd5785cd00a16e8bfd05e910c6d920
Lane Schwartz <dowobeha@gmail.com>
2023-04-21 09:53:24 -0800

    Adding reference sample code

--- week14/references.cpp
+++ week14/references.cpp
@@ -1,10 +1,31 @@
+#include <iostream>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+int AK = 49;  // This variable has *global* scope
 
 class Library {
-  ///...
+
+public:
+
+  Library() {
+    cerr << "Calling Library() default constructor" << endl;
+  }
+
+  Library(const Library& original) {
+    cerr << "Calling Library copy constructor" << endl;
+  }
+  
+  ~Library() {
+    cerr << "Library object went out of scope, and now doesn't exist" << endl;
+  }
+
 };
 
 
-int booksInLibrary(Library& library) {
+int booksInLibrary(Library& bob) {
 
 
   return 27050;
@@ -12,10 +33,21 @@ int booksInLibrary(Library& library) {
 
 int main() {
 
-  Library l;
+  // Scope describes where an object exists
+  
+  Library library;
   // add a bunch of books
 
   int x = booksInLibrary(library);
+
+
+  for (int z{0}; z < 10; z += 1) {
+
+    cout << z << endl;
+
+  }
+
+  // cout << z;  z is *not* in scope here
   
   
   return 0;
